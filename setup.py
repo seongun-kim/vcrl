b33655095005e7b624889abec740f8e120353749
leekwoon <leekwoon@unist.ac.kr>
2023-07-15 19:50:51 +0900

    update email

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='vcrl',
     description='Code for the paper "Variational Curriculum Reinforcement Learning for Unsupervised Discovery of Skills"',
     author='Seongun Kim, Kyowoon Lee, and Jaesik Choi',
+    author_email='leekwoon@unist.ac.kr, seongun@kaist.ac.kr and jaesik.choi@kaist.ac.kr',
     version='0.1',
     packages=find_packages(),
     license='MIT License',
